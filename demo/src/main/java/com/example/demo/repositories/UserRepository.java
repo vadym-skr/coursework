90e1af9142431c482c13cd862c002bf86776b0ee
Vadim <vadim@Home>
2021-05-12 00:28:19 +0300

    third and one commit : ТЕСТОВА функція виводу користувачів!

--- demo/src/main/java/com/example/demo/repositories/UserRepository.java
+++ demo/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -2,20 +2,27 @@ package com.example.demo.repositories;
 
 import com.example.demo.entity.User;
 import jdk.dynalink.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 
 // CrudRepository - Інтерфейс для спільних операцій CRUD з репозиторієм певного типу.
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("Select u FROM User u WHERE u.username = :username")
     User findUserByUsername(@Param("username") String username);
 
     @Query("Select u FROM User u WHERE u.id = :id")
     User findUserById(@Param("id") Integer id);
+
+    @Query(
+            value = "SELECT user_id, username, password, enabled, email FROM (SELECT @n:=@n+1 as rownum, u.* FROM users u) as t, (SELECT @n := 0)n;",
+            nativeQuery = true)
+    List<User> findUserByNumberToNumber(@Param("from") int from, @Param("to") int to, @Param("amount") int amount);
 
     List<User> findUsersByUsername(String username);
 
